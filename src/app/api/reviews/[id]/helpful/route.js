e443db0d55d56f23347ecc10c2e4dcab354cfb4a
IDOKO EMMANUEL <eemmanuel.idoko@gmail.com>
2025-09-13 17:43:05 +0100

    deployment fix

--- src/app/api/reviews/[id]/helpful/route.js
+++ src/app/api/reviews/[id]/helpful/route.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/route.js';
+import { authOptions } from '../../../auth/[...nextauth]/route.js';
 import { db } from '@/../firebase-admin.config';
 
 export async function POST(request, { params }) {
